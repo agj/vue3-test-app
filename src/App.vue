55aea07ab91033d50d3f4c98a4903bc9f4cd8aa4
agj <ale@agj.cl>
2021-04-07 10:46:46 -0400

    Probando estilos.

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,166 @@
   <article class="container">
     <h1>Hechos inútiles sobre Rick &amp; Morty</h1>
     <section class="box">
-      <h2>Cuántas veces se repite una letra en el nombre de…</h2>
-      <div class="card">L en lugares</div>
-      <div class="card">E en episodios</div>
-      <div class="card">C en personajes</div>
+      <div class="description">
+        Cuántas veces se repite una letra en el nombre de…
+      </div>
+      <div class="items">
+        <div class="card">L en lugares</div>
+        <div class="card">E en episodios</div>
+        <div class="card">C en personajes</div>
+      </div>
     </section>
-    <section class="box">
-      <p>
+    <section class="ep-char-origins box">
+      <div class="description">
         Para cada capítulo, una lista con el origen de todos los personajes que
         aparecen en ese capítulo.
-      </p>
-      <section class="card">
-        <h3>S01E01 Pilot</h3>
-        <ul>
-          <li>Earth</li>
-          <li>Mars</li>
-          <li>Jupiter</li>
-        </ul>
-      </section>
-      <section class="card">
-        <h3>S01E01 Pilot</h3>
-        <ul>
-          <li>Earth</li>
-          <li>Mars</li>
-          <li>Jupiter</li>
-        </ul>
-      </section>
+      </div>
+      <div class="items">
+        <section class="card">
+          <div class="header">
+            <div class="title">Pilot</div>
+            <div class="number">T01E01</div>
+          </div>
+          <div class="content">
+            <ul class="locations">
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+            </ul>
+          </div>
+        </section>
+        <section class="card">
+          <div class="header">
+            <div class="title">Pilot</div>
+            <div class="number">T01E01</div>
+          </div>
+          <div class="content">
+            <ul class="locations">
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+            </ul>
+          </div>
+        </section>
+        <section class="card">
+          <div class="header">
+            <div class="title">Pilot</div>
+            <div class="number">T01E01</div>
+          </div>
+          <div class="content">
+            <ul class="locations">
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+            </ul>
+          </div>
+        </section>
+        <section class="card">
+          <div class="header">
+            <div class="title">Pilot</div>
+            <div class="number">T01E01</div>
+          </div>
+          <div class="content">
+            <ul class="locations">
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+            </ul>
+          </div>
+        </section>
+        <section class="card">
+          <div class="header">
+            <div class="title">Pilot</div>
+            <div class="number">T01E01</div>
+          </div>
+          <div class="content">
+            <ul class="locations">
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+            </ul>
+          </div>
+        </section>
+        <section class="card">
+          <div class="header">
+            <div class="title">Pilot</div>
+            <div class="number">T01E01</div>
+          </div>
+          <div class="content">
+            <ul class="locations">
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+            </ul>
+          </div>
+        </section>
+        <section class="card">
+          <div class="header">
+            <div class="title">Pilot</div>
+            <div class="number">T01E01</div>
+          </div>
+          <div class="content">
+            <ul class="locations">
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+            </ul>
+          </div>
+        </section>
+        <section class="card">
+          <div class="header">
+            <div class="title">Pilot</div>
+            <div class="number">T01E01</div>
+          </div>
+          <div class="content">
+            <ul class="locations">
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+            </ul>
+          </div>
+        </section>
+        <section class="card">
+          <div class="header">
+            <div class="title">Pilot</div>
+            <div class="number">T01E01</div>
+          </div>
+          <div class="content">
+            <ul class="locations">
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+              <li>Earth</li>
+              <li>Mars</li>
+              <li>Jupiter</li>
+            </ul>
+          </div>
+        </section>
+      </div>
     </section>
   </article>
 </template>
@@ -43,30 +177,85 @@ export default defineComponent({
 
 <style>
 :root {
-  --color-back: hsl(43, 100%, 61.5%);
-  --color-border: hsl(42.8, 64.2%, 50.6%);
-  font-size: 14pt;
+  --color-back: hsl(0, 0%, 91.3%);
+  --color-box: hsl(0, 0%, 94.8%);
+  --space-l: 1.7rem;
+  --space-m: 1rem;
+  --space-s: 0.5rem;
+  --space-xs: 0.2rem;
+  --font-l: 22pt;
+  --font-m: 14pt;
+  --font-s: 10pt;
+
+  font-size: var(--font-m);
+  line-height: 1.3;
 }
 html,
 body {
   margin: 0;
   padding: 0;
+  background-color: var(--color-back);
 }
 * {
   box-sizing: border-box;
 }
+
+ul,
+li {
+  padding: 0;
+  margin: 0;
+}
+
+h1 {
+  font-size: var(--font-l);
+  margin: 0;
+  padding: 0;
+}
+
 .container {
   font-family: Helvetica, Arial, sans-serif;
   max-width: 50rem;
-  padding: 1rem;
+  padding: var(--space-m);
   margin: 0 auto;
 }
 .box {
   padding: 1rem;
-  margin: 1rem 0;
-  background-color: var(--color-back);
-  border-width: 5px;
-  border-color: var(--color-border);
-  border-style: solid;
+  margin: var(--space-m) 0;
+  background-color: var(--color-box);
+}
+.items {
+  display: grid;
+  grid-template-columns:
+    1fr
+    1fr
+    1fr
+    1fr;
+  gap: var(--space-xs);
+}
+
+.card .header {
+  padding: var(--space-s) var(--space-s) var(--space-xs) var(--space-s);
+  border-bottom: black solid 1px;
+}
+.card .content {
+  padding: var(--space-s);
+}
+
+/* EP-CHAR-ORIGINS */
+
+.ep-char-origins .card {
+  background-color: hsl(126.3, 72.7%, 88.3%);
+}
+.ep-char-origins .card ul {
+  display: flex;
+  flex-wrap: wrap;
+}
+.ep-char-origins .card li {
+  list-style: none;
+  display: block;
+  margin-right: var(--space-s);
+}
+.ep-char-origins .card li:last-child {
+  margin-right: 0;
 }
 </style>
